7396244507f69cd6f71e446e06b5c74f9e750a77
unknown <christianyt@KPNO-IT-04.pif.co.id>
2020-10-02 20:02:01 +0700

    post revisi

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -38,31 +38,83 @@ check('text','Text required').not().isEmpty()
         res.send(400).json({msg:error.message})
         
     }
-
-
-
-
 });
 
+
+
+
+
+router.get('/', auth, async (req,res)=> {
+    try {
+        const allpost = await Post.find().sort({date: -1 }).populate('user', ['name', 'avatar'])
+        res.json(allpost)
+    
+    } catch (error) {
+        console.error(error.message)
+        res.status(400).send('Server Error')
+    }}
+)
+
+
+
+
 router.get('/mypost', auth, async (req,res)=> {
-
-
         try {
             const allpost = await Post.find({user: req.user.id}).populate('user', ['name', 'avatar'])
-
             res.json(allpost)
-        
         
         } catch (error) {
             console.error(error.message)
             res.status(400).send('Server Error')
-    
-}
+        }})
 
 
-}
 
 
-)
 
+
+
+
+
+
+    router.get('/:id', auth, async (req,res)=> {
+        try {
+            const post = await Post.findById(req.params.id)
+            res.json(post)
+            
+            } catch (error) {
+                console.error(error.message)
+        
+                if (error.kind == "ObjectId") {
+                    return res.status(400).json({ msg: 'Parameter not Object ID' })
+                }
+                res.status(400).send('Server Error')
+            }}
+    )
+        
+    router.delete('/:id', [auth,[
+        check('email','Email wajib').not().isEmpty()
+    ]], async (req,res)=> {
+        try {
+            const post = await Post.findOne(req.body.email)
+
+            if (!post){
+                return res.status(500).json({err:'Post tidak ada'})
+            }
+            if (req.user.id !== post.user.toString() ) {
+                return res.status(401).json({err:'User error'})
+            }
+
+            await post.remove()
+            res.status(200).send('Deleted')
+            
+            } catch (error) {
+              console.error(error.message)
+                if (error.kind == "ObjectId") {
+                      return res.status(400).json({ msg: 'Parameter not Object ID' })
+                    }
+                    res.status(400).send('Server Error')
+                }}
+    )    
+        
 module.exports =  router
